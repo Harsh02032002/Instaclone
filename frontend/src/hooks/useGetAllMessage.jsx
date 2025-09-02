00ffc97bf33a01631491d5d08784f3ece8914f9b
Harsh <harshdeepbca503@gmail.com>
2025-09-02 23:32:12 +0530

    Update useGetAllMessage.jsx

--- frontend/src/hooks/useGetAllMessage.jsx
+++ frontend/src/hooks/useGetAllMessage.jsx
@@ -1,22 +1,17 @@
 import { setMessages } from "@/redux/chatSlice";
+import { setPosts } from "@/redux/postSlice";
+import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 
 const useGetAllMessage = () => {
     const dispatch = useDispatch();
-    const { selectedUser } = useSelector(store => store.auth);
-
+    const {selectedUser} = useSelector(store=>store.auth);
     useEffect(() => {
-        if(!selectedUser) return;
-
         const fetchAllMessage = async () => {
             try {
-                const res = await axios.get(
-                    `https://instaclone-axnu.onrender.com/api/v1/message/all/${selectedUser._id}`,
-                    { withCredentials: true }
-                );
-                if(res.data.success){
+                const res = await axios.get(`https://instaclone-g9h5.onrender.com/api/v1/message/all/${selectedUser?._id}`, { withCredentials: true });
+                if (res.data.success) {  
                     dispatch(setMessages(res.data.messages));
                 }
             } catch (error) {
@@ -24,6 +19,6 @@ const useGetAllMessage = () => {
             }
         }
         fetchAllMessage();
-    }, [selectedUser, dispatch]);
+    }, [selectedUser]);
 };
 export default useGetAllMessage;
