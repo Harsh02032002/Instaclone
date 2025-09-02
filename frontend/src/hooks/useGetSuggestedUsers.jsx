0f74fdb17aa3118f9311cfe0a603e15047c94778
Harsh <harshdeepbca503@gmail.com>
2025-09-02 23:21:36 +0530

    Update useGetSuggestedUsers.jsx

--- frontend/src/hooks/useGetSuggestedUsers.jsx
+++ frontend/src/hooks/useGetSuggestedUsers.jsx
@@ -3,14 +3,16 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-
 const useGetSuggestedUsers = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         const fetchSuggestedUsers = async () => {
             try {
-                const res = await axios.get('https://instaclone-axnu.onrender.com/api/v1/user/suggested', { withCredentials: true });
-                if (res.data.success) { 
+                const res = await axios.get(
+                    'https://instaclone-axnu.onrender.com/api/v1/user/suggested',
+                    { withCredentials: true }
+                );
+                if (res.data.success) {
                     dispatch(setSuggestedUsers(res.data.users));
                 }
             } catch (error) {
@@ -20,4 +22,5 @@ const useGetSuggestedUsers = () => {
         fetchSuggestedUsers();
     }, []);
 };
+
 export default useGetSuggestedUsers;
