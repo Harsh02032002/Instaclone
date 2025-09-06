f3899074782f2db5e886461f88f6d7cebeecc3d9
Harsh <harshdeepbca503@gmail.com>
2025-09-06 17:21:19 +0530

    Update ChatPage.jsx

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -17,7 +17,7 @@ const ChatPage = () => {
 
     const sendMessageHandler = async (receiverId) => {
         try {
-            const res = await axios.post(`https://instaclone-axnu.onrender.com/api/v1/message/send/${receiverId}`, { textMessage }, {
+            const res = await axios.post(`https://instaclone-63mb.vercel.app/api/v1/message/send/${receiverId}`, { textMessage }, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
