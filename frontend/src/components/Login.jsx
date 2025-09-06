656e5a5f5f546eb9e8223807f72b641585d80060
Harsh <harshdeepbca503@gmail.com>
2025-09-06 17:25:58 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "https://instaclone-axnu.onrender.com/api/v1/user/login",
+        "https://instaclone-63mb.vercel.app/api/v1/user/login",
         input,
         {
           headers: {
